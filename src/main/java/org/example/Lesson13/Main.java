35578f475716f3ab7efea8a130b5bbc3529f9322
Andreybukh2 <121665325+Andreybukh2@users.noreply.github.com>
2023-02-19 15:05:04 +0200

    Пример входного потока без Сканнера и перевод bate в char

---
+++ src/main/java/org/example/Lesson13/Main.java
@@ -0,0 +1,23 @@
+package org.example.Lesson13;
+
+import java.io.IOException;
+
+// Рассмотрим основыные методы и это пример input ыекшьф
+// Прочитаем байты с клавиатуры. Мы берем какой-то стрим input в нашем случае, который беорет данные из нашей консоли,
+// блокируется консоль
+// System.in. - принимае входящий поток и имеет различные методы
+// Немного перестроили под данные bate;
+public class Main {
+    public static void main(String[] args) throws IOException {
+    byte[] info = new byte[12]; // массив из 12 символов
+        System.out.print("Enter some text");
+        System.in.read(info);
+        char[] result = new char[12];
+        for (int i = 0; i < info.length; i++){
+            result[i] = (char)info[i];
+            System.out.print(info[i]);
+        }
+        String res = new String(result);
+        System.out.println(" You are enter " + res);
+    }
+}
