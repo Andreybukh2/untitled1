c7cd637655c61ec640fa0cc72174ab17b53b482b
Andreybukh2 <121665325+Andreybukh2@users.noreply.github.com>
2023-02-20 18:47:35 +0200

    Пример рандомного вывода потоков

---
+++ src/main/java/org/example/Lesson13/Potoki.java
@@ -0,0 +1,16 @@
+package org.example.Lesson13;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Potoki {
+    public static void main(String[] args) {
+        Ticker ticker1 = new Ticker("Potok1");
+        Ticker ticker2 = new Ticker("Potok2");
+//        ticker1.run();
+//        ticker2.run();
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        executorService.submit(ticker1);
+        executorService.submit(ticker2);
+    }
+}
---
+++ src/main/java/org/example/Lesson13/Ticker.java
@@ -0,0 +1,19 @@
+package org.example.Lesson13;
+
+public class Ticker implements Runnable {
+    private final String name;
+    public Ticker(String name){
+        this.name = name;
+    }
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(name + "- " + i);
+            try {
+                Thread.sleep(600); // Класс поток и фукция sleep
+            } catch (InterruptedException e) { // Когда поток прерывается
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
